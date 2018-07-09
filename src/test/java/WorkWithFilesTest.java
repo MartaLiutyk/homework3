41995b75ec885408b02240bcc56698ec759e376c
Marta Liutyk <melodika99@gmail.com>
2018-07-09 12:30:27 +0300

    all homework done - test for void method added

--- src/test/java/WorkWithFilesTest.java
+++ src/test/java/WorkWithFilesTest.java
@@ -1,6 +1,12 @@
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class WorkWithFilesTest {
     @Test
@@ -8,4 +14,11 @@ public class WorkWithFilesTest {
         String information = ".";
         assertNotNull(information);
     }
+
+    @Test
+    public void putDataIntoFile() throws IOException {
+        Path file = Paths.get("C:/Users/melod/hw3/helloworld/filework");
+        assertTrue(file.toFile().exists());
+    }
+
 }
